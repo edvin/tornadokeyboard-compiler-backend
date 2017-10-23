c5d785ffcf7c49706fbfdf60822adbad05c3eab9
Edvin Syse <es@syse.no>
2017-10-23 07:57:13 +0200

    Fill 62 keys in each layer

--- src/main/kotlin/no/tornado/keyboard/models/Layer.kt
+++ src/main/kotlin/no/tornado/keyboard/models/Layer.kt
@@ -8,9 +8,12 @@ enum class LayerToggleType(val actionName: String) {
     Momentary("ACTION_LAYER_MOMENTARY")
 }
 
-data class Layer(val name: String, val keys: List<Key>) {
-    constructor(json: JsonObject) : this (
-            json.getString("name"),
-            json.array("keys")?.map { Key(it as JsonObject) } ?: ArrayList()
-    )
+data class Layer(val name: String) {
+    val keys = MutableList(62) { Key(KeyCode.KC_TRNS) }
+
+    constructor(json: JsonObject) : this(json.getString("name")) {
+        json.array("keys")
+                ?.map { Key(it as JsonObject) }
+                ?.forEachIndexed { index, key -> keys[index] = key }
+    }
 }
